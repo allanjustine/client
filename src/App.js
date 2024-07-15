1f7536bd2b5fae819993a2b2343366afb015b5b3
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2024-07-15 17:02:16 +0800

    asdasdasd

--- src/App.js
+++ src/App.js
@@ -30,6 +30,18 @@ function App() {
           <Route path='/' element={<LogIn />}></Route>
           <Route path='/forgot' element={<Forgot />}></Route>
           <Route path='/signup' element={<SignUp />}></Route>
+          <Route path='/dashboard' element={<DashBoard />}></Route>
+          <Route path='/unit' element={<Unit />}></Route>
+          <Route path='/set' element={<Set />}></Route>
+          <Route path='/profile' element={<Profile />}></Route>
+          <Route path='/computers' element={<Computers />}></Route>
+          <Route path='/qr' element={<QrC />}></Route>
+          <Route path='/allunits' element={<AllUnits />}></Route>
+          <Route path='/computers/:id' element={<Extract />} />
+          <Route path='/user' element={<User/>}/>
+          <Route path='/add' element={<Add/>}/>
+          <Route path='*' element={<NotFound />} />
+          <Route path='/print' element={<PrintInformation />} />
         </Route>
 
         <Route element={<ChangePassword />}>
